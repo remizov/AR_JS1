a9e672edc2a0f44d4227be109608b43cb31c0c40
Alexey Remizov <alexey.remizov@gmail.com>
2019-04-24 09:38:13 +0300

    test1

--- b.js
+++ b.js
@@ -1,2 +1,4 @@
 // Hello world!
-alert ("Hello");
+// https://remizov.github.io/AR_JS1/b.js
+alert("Hello");
+console.log("wow");
---
+++ loader.js
@@ -0,0 +1,1 @@
+function ARLoader() {var Script1 = document.createElement('script'); Script1.src = "https://remizov.github.io/AR_JS1/b.js";  document.body.appendChild(Script1);} ARLoader() ;
